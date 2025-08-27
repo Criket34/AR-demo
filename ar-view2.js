9b9d11cad934e2429ec9918b05475e180c2a097e
Criket34 <168623069+Criket34@users.noreply.github.com>
2025-08-27 19:51:09 +0900

    Add files via upload

--- ar-view2.js
+++ ar-view2.js
@@ -1,25 +1,28 @@
 import * as THREE from "three";
 import { MindARThree } from "mindar-image-three";
 
+let mindarThree;
+let renderAnimationId = null;
+
 const container = document.getElementById("container");
 
-const mindarThree = new MindARThree({
+mindarThree = new MindARThree({
   container: container,
-  imageTargetSrc: "./doki-color.mind",
+  imageTargetSrc: "./doki-color.mind", // ここでマーカー指定
 });
 
 const { renderer, scene, camera } = mindarThree;
-
 const anchor = mindarThree.addAnchor(0);
+
 const geometry = new THREE.PlaneGeometry(1.5, 1.5);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ffff, transparent: true, opacity: 0.5 });
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ffff,
+  transparent: true,
+  opacity: 0.5,
+});
 const plane = new THREE.Mesh(geometry, material);
 anchor.group.add(plane);
 
-const startButton = document.getElementById("startButton");
-const stopButton = document.getElementById("stopButton");
-
-let renderAnimationId;
 const update = () => {
   renderer.render(scene, camera);
   renderAnimationId = requestAnimationFrame(update);
@@ -30,10 +33,11 @@ const start = async () => {
   update();
 };
 
-startButton.addEventListener("click", () => {
+document.getElementById("startButton").addEventListener("click", () => {
   start();
 });
-stopButton.addEventListener("click", () => {
+
+document.getElementById("stopButton").addEventListener("click", () => {
   mindarThree.stop();
   cancelAnimationFrame(renderAnimationId);
 });
